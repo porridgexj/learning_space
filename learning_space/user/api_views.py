a087894bedf5c42780bc1ad4774c66f09b7056fb
Shijie Zhou <2981151z@student.gla.ac.uk>
2025-03-04 13:14:03 +0000

    feat: optimize file structure

--- learning_space/user/api_views.py
+++ learning_space/user/api_views.py
@@ -6,7 +6,7 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
-from ..models import User
+from .models import User
 import json
 import hashlib
 import re
