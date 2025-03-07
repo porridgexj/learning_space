efbe6f483b7a449d2ddaa8bacf07f72caf151cdc
S ZHOU <1543699447@qq.com>
2025-03-07 23:17:34 +0000

    feat: update index

--- learning_space/static/js/index.js
+++ learning_space/static/js/index.js
@@ -1,24 +1,39 @@
 let map = null;
-let currentMapCenter = {lat: 55.8668275, lng: -4.2514823};
+let currentMapCenter = { lat: 55.8668275, lng: -4.2514823 };
 let mapType = window.localStorage.getItem('mapType') ?? 'normal';
 let mapSource = {
-    'normal': 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    'satellite': 'https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png?api_key=c133c1b3-dc26-4883-b995-4358b31c43d4',
+  'normal': 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  'satellite': 'https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png?api_key=c133c1b3-dc26-4883-b995-4358b31c43d4',
 };
 
 function drawMap() {
-    if (map) map.remove();
-    const {lat, lng} = currentMapCenter;
-    map = L.map('map-container').setView([lat, lng], 14);
-    L.tileLayer(mapSource[mapType], {
-        maxZoom: 19,
-    }).addTo(map);
+  if (map) map.remove();
+  const { lat, lng } = currentMapCenter;
+  map = L.map('map-container').setView([lat, lng], 14);
+  L.tileLayer(mapSource[mapType], {
+    maxZoom: 19,
+  }).addTo(map);
+}
+
+function getSpaceList() {
+  const spaceList = Array(20).fill({
+    name: 'Space 1',
+    rating: '★★★★★',
+  });
+  $('#space-container').empty();
+  for (space of spaceList) {
+    const newElement = $('#space-item-template').clone();
+    newElement.show();
+    newElement.removeAttr('id');
+    newElement.find('.space-name').text(space.name);
+    newElement.find('.space-rating').text(space.rating);
+    $('#space-container').append(newElement);
+  }
 }
 
 function init() {
-    drawMap();
-    console.log(map);
-    console.log('???')
+  drawMap();
+  getSpaceList();
 }
 
 init();
