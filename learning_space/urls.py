84a3a8a22e188fead2253cc5bc1938ea91674cf7
S ZHOU <1543699447@qq.com>
2025-03-14 22:42:59 +0000

    fix: /api suffix error

--- learning_space/urls.py
+++ learning_space/urls.py
@@ -14,29 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from learning_space.user import api_views
-from learning_space.user.api_urls import urlpatterns as user_urls  
+from learning_space.user.api_urls import urlpatterns as user_urls
 from learning_space.space.api_urls import urlpatterns as space_urls
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index),
-    # path('api/user', include('learning_space.user.api_urls')),
-    # path('api/space', include('learning_space.space.api_urls')),
-    path('api/', include(user_urls)),
-    path('api/', include(space_urls)),
-    # path('api/register/', api_views.register, name='register'),
-    # path('api/login/', api_views.login, name='login'),
-    # path('api/', include('learning_space.user.api_urls')),
-    path('login', views.login),
-    path('reserve/<int:id>/', views.reserve),
-    path('comments/<int:id>/', views.comments),
-    path('favours', views.favours),
+    path("admin/", admin.site.urls),
+    path("api/", include(user_urls)),
+    path("", include(space_urls)),
+    path("", views.index),
+    path("login", views.login),
+    path("reserve/<int:id>/", views.reserve),
+    path("comments/<int:id>/", views.comments),
+    path("favours", views.favours),
 ]
-
-# urlpatterns.extend(user_urls)
-# urlpatterns.extend(space_urls)
