171b4a88f0dc111b2c3399424047b0387e458402
lulu990112 <ruilutang2023@163.com>
2025-03-18 07:09:30 +0800

    Update api_urls.py
    change  path("add-favourite", api_views.add_favourite_space),
      to  path("add-favourite", api_views.add_favourite_space, name="add_favourite_space"),

--- learning_space/user/api_urls.py
+++ learning_space/user/api_urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("bookings", api_views.get_booking_history, name="get_booking_history"),
     path("comments", api_views.get_comments, name="get_comments"),
     path("comments/submit", api_views.submit_comment, name="submit_comment"),
-    path("add-favourite", api_views.add_favourite_space),
+    # path("add-favourite", api_views.add_favourite_space),
+    path("add-favourite", api_views.add_favourite_space, name="add_favourite_space"),
     path("del-favourite", api_views.delete_favourite_space),
 ]
