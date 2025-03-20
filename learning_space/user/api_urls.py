4b4f115bdc6c0827ebc709b0d4ba9fa07adfcbd0
Xuebin Ren <424447050@qq.com>
2025-03-20 14:45:12 +0000

    fix unit test

--- learning_space/user/api_urls.py
+++ learning_space/user/api_urls.py
@@ -3,8 +3,8 @@ from . import api_views
 
 urlpatterns = [
     path("register", api_views.register, name="register"),
-    path("login", api_views.login, name="login"),
-    path("logout", api_views.logout),
+    path("login", api_views.login, name="login_api"),
+    path("logout", api_views.logout, name="logout"),
     path("favourites", api_views.get_favourite_spaces, name="get_favourite_spaces"),
     path("bookings", api_views.get_booking_history, name="get_booking_history"),
     path("comments", api_views.get_comments, name="get_comments"),
